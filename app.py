2c3b01b2440af0b87157191eed3c81cc504c51a4
lucas <luquinhas.2022.samuel@gmail.com>
2026-09-06 10:52:40 -0300

    primeira etapa

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/cadastro", methods=["GET","POST" ])
+def cadastro():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        cpf = request.form["cpf"]
+        return render_template("cadastro_realizado.html", nome=nome , cpf=cpf)
+    return render_template("Cadastro.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
